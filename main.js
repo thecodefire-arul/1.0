1fb61948bc412fadca6548600fb126568d007d3a
Aryan <aryansharma11223@gmail.com>
2020-05-13 09:47:32 -0700

    Progress Bar

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+//Progress Bar
+
+window.onscroll = function() {progressBar()};
+
+function progressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
 //Navbar
 
 $(document).ready(function(){
